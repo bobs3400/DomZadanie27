fd2b96d2a444139a3347fa1ee55fc0e9ecc12124
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-22 20:53:33 +1000

    27

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int GetSum(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        int result = n % 10;
+        sum = sum + result;
+        n = n / 10;
+    }
+    return sum;
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {n} равна {GetSum(n)}");
